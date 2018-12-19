46d35d49d05afcad74937a417652e0c66485c33a
len0rd <tylerm15@gmail.com>
2018-12-18 23:05:13 -0700

    #2 turned out i was already using c++11 and moving to aptitude cmake was trivial lol

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 This package exposes a Sony a6000 to a ROS network by using [GPhoto2](https://github.com/gphoto/gphoto2).
 
+## Dependencies
+
+This package requires a number of dependencies that are not installed in a default ROS linux environment. The main dependency is:
+
+- [libgphoto2](https://github.com/gphoto/libgphoto2) (Developed on the 2.5.21 release)
+
+The `install-deps.sh` scripts in the root of the repository will install a verified working release
+of libgphoto2. It should work on ARM or x86 linux Ubuntu 16.04. (Currently untested on other versions).
+
 ## Setup
 
 Configure your a6000 in 'PC Remote' mode by navigating: `Menu -> Suitcase tab on top right -> Page 4 -> USB Connection -> PC Remote`.
--- install_deps.sh
+++ install_deps.sh
@@ -1,32 +1,24 @@
-# dependencies for the dependency. Need these to build libgphoto2 from src
-sudo apt -y install autoconf autopoint automake pkg-config gettext libtool libusb-1.0-0-dev libexif-dev
+#1/bin/bash
 
-wget https://github.com/gphoto/libgphoto2/archive/libgphoto2-2_5_21-release.tar.gz
+function installLibGphoto() {
+    # dependencies for the dependency. Need these to build libgphoto2 from src
+    sudo apt -y install autoconf autopoint automake pkg-config gettext libtool libusb-1.0-0-dev libexif-dev
 
-tar -xvf libgphoto2-2_5_21-release.tar.gz
+    wget https://github.com/gphoto/libgphoto2/archive/libgphoto2-2_5_21-release.tar.gz
 
-cd libgphoto2-libgphoto2-2_5_21-release/
+    tar -xf libgphoto2-2_5_21-release.tar.gz
 
-autoreconf --install --symlink
-./configure --prefix=/usr/local
-make
-sudo make install
+    cd libgphoto2-libgphoto2-2_5_21-release/
 
-# need newer version of cmake than what comes with 16.04 from aptitude
-#NOTE this only works for x86 processors - need something different for ARM
-arch="$(uname -m)"
+    autoreconf --install --symlink
+    ./configure --prefix=/usr/local
+    # make
+    # sudo make install
+    
+    cd ../
+    rm -rf libgphoto2-libgphoto2-2_5_21-release/
+    rm libgphoto2-2_5_21-release.tar.gz
+    echo "===DONE!!==="
+}
 
-if [ ${arch} = "x86_64" ]; then
-
-    sudo apt -y remove cmake
-    cd /opt/
-    sudo wget https://github.com/Kitware/CMake/releases/download/v3.13.1/cmake-3.13.1-Linux-x86_64.sh
-    sudo chmod +x cmake-3.13.1-Linux-x86_64.sh 
-
-    #running this askes for a bunch of yeses
-    sudo ./cmake-3.13.1-Linux-x86_64.sh 
-
-    sudo ln -s /opt/cmake-3.13.1-Linux-x86_64/bin/* /usr/local/bin
-else
-    echo "Hey there. Tyler knows how to install cmake from source on non-x86 platforms, but he's lazy and hasn't put it in this script yet..."
-fi
+installLibGphoto
