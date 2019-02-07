4b11b349c5349a405d4662b95e948422de2eaa8d
len0rd <tylerm15@gmail.com>
2019-02-07 09:27:10 -0700

    add user to dialout and plugdev in install deps. remove useless print from camera_connector

--- install_deps.sh
+++ install_deps.sh
@@ -4,6 +4,10 @@ function installLibGphoto() {
     # dependencies for the dependency. Need these to build libgphoto2 from src
     sudo apt -y install autoconf autopoint automake pkg-config gettext libtool libusb-1.0-0-dev libexif-dev
 
+    echo "===We need to add you to the plugdev and dialout group, otherwise we cant connect to the camera==="
+    sudo adduser $USER dialout
+    sudo adduser $USER plugdev
+
     wget https://github.com/gphoto/libgphoto2/archive/libgphoto2-2_5_21-release.tar.gz
 
     tar -xf libgphoto2-2_5_21-release.tar.gz
